04c8b37b1a123d4e4d67928f0e1d51204172d362
Rafli <raflidaffapratama@gmail.com>
2024-09-09 22:22:56 +0700

    Penambahan Profile

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profile', [ProfileController::class, 'profile']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
